3e72ebc2ec1a1c0c17d0f3e3d4500023ab7a2a80
NetraWalekar <netrawalekar6@gmail.com>
2024-09-09 16:29:55 +0530

    files added

--- src/Views/Contact/Contact.js
+++ src/Views/Contact/Contact.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import './contact.css'
+import helpimg from './help.jpg'
+import callimg from './call.png'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className='big'>
+      <div className='container'>
+        <div className='sales'>
+          <div><h1>Contact Us</h1></div>
+          <div className='innerbox1'>
+            <div>          <img src={callimg} alt='img' className='callimg'></img>
+            </div>
+            <div style={{textAlign:'center',fontSize:'30px'}}>Talk to a member of our sales team.</div>
+            <div style={{textAlign:'center',margin:'10px'}}>We'll help you find out right  product at right pricing.</div>
+            <button></button>
+          </div>
+        </div>
+        <div>
+          <img src={helpimg} alt='img' className='img'></img>
+        </div>
+      </div>
+    </div>
   )
 }
 

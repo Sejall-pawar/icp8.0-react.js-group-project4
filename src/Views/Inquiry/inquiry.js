9ef5c415710f0017d70217755369df2f6236b89f
NetraWalekar <netrawalekar6@gmail.com>
2024-09-11 13:06:04 +0530

    inquiry page

---
+++ src/Views/Inquiry/inquiry.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './inquiry.css'
+import Input from "../../Components/input/input";
+
+function Inquiry(){
+    return(
+        <div>
+            <div className="container-inquiry">
+                <div><Input /></div>
+            </div>
+        </div>
+    )
+}
+
+export default Inquiry;

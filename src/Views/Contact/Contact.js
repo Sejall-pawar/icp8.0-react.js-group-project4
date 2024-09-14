087d5dc2e80ae8938cc9c8800f6c7ee5ce1b807d
NetraWalekar <netrawalekar6@gmail.com>
2024-09-14 15:32:45 +0530

    updates

--- src/Views/Contact/Contact.js
+++ src/Views/Contact/Contact.js
@@ -1,31 +1,34 @@
 import React from 'react'
 import './contact.css'
-import helpimg from './help.jpg'
-import callimg from './call.png'
+import sales from './sales.png'
 import Button1 from '../../Components/button/button'
 import Footer from '../../Components/Footer/Footer'
 import Header from '../../Components/Header/Header'
-import Inquiry from '../Inquiry/inquiry'
 import { Link} from 'react-router-dom'
+import location from './location.png'
+import call from './call.png'
+import mail from './mail.png'
 
 function Contact() {
   return (
     <div>
       <Header />
-      <div className='container-1'>
-        <div className='sales'>
-          <div><h1 className='title'>Contact Us</h1></div>
+      <div>
+      <div><h1 className='title'>Contact Us</h1></div>
+        <div className='container-1'>
+          <div className='innerbox'>
+             <div className='items'><img src={location} alt='img' className='icons'/> 1 &2, Agarwal Market, Station Rd, Opp.rly St., Vile Parle (e) Mumbai Maharashtra</div>
+             <div className='items'><img src={call} alt='img' className='icons'/> +91 2226146184</div>
+             <div className='items'><img src={mail} alt='img' className='icons'/> rentifyhub@gmail.com</div>
+          </div>
           <div className='innerbox1'>
-            <div> <img src={callimg} alt='img' className='callimg'></img>
+            <div><img src={sales} alt='img' className='callimg'></img>
             </div>
-            <div style={{textAlign:'center',fontSize:'30px',color:'#405D72',}}>Talk to a member of our sales team.</div>
+            <div style={{textAlign:'center',fontSize:'30px',color:'#405D72',padding:'10px'}}>Talk to a member of our sales team.</div>
             <div style={{textAlign:'center',margin:'10px',color:'#758694'}}>We'll help you find out right  product at right pricing.</div>            
-            <div style={{margin:'35px'}}><Button1 text='Contact Sales'/></div>
-            <div style={{margin:'35px'}}><Link to='/inquiry'><Button1 text='Send Inquiry'/></Link></div>
-           </div>
+            <div className='button1'><Button1 text='Contact Sales'/></div>
+            <div className='button1'><Link to='/inquiry'><Button1 text='Send Inquiry'/></Link></div>
         </div>
-        <div>
-          <img src={helpimg} alt='img' className='img'></img>
         </div>
       </div>
       <Footer />

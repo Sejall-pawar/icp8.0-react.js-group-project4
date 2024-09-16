86ca90fe004db430cf51a93b230068363f67dde1
Sejall-pawar <sejalpawar463@gmail.com>
2024-09-16 22:59:46 +0530

    added the active menu button

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.css'; 
 
 function Header() {
@@ -26,19 +26,60 @@ function Header() {
         />
         <nav>
           <ul className={isOpen ? 'open' : ''}>
-            <li><Link to="/" className='nav-names'>Home</Link></li>
+            <li>
+              <NavLink 
+                exact 
+                to="/" 
+                className='nav-names' 
+                activeClassName="active"
+              >
+                Home
+              </NavLink>
+            </li>
             <li className="dropdown">
-              <Link to="#" className='nav-names dropbtn'>Categories</Link>
+              <span className='nav-names dropbtn'>Categories</span>
               <div className="dropdown-content">
-                <Link to="/furniture">Furniture</Link>
-                <Link to="/garments">Garments</Link>
-                <Link to="/electronics">Electronics</Link>
+                <NavLink to="/furniture" activeClassName="active">Furniture</NavLink>
+                <NavLink to="/garments" activeClassName="active">Garments</NavLink>
+                <NavLink to="/electronics" activeClassName="active">Electronics</NavLink>
               </div>
             </li>
-            <li><Link to="/contact" className='nav-names'>Contact</Link></li>
-            <li><Link to="/registration" className='nav-names'>Registration</Link></li>
-            <li><Link to="/reviews" className='nav-names'>Reviews</Link></li>
-            <li><Link to="/inquiry" className='nav-names'>Inquiry</Link></li>
+            <li>
+              <NavLink 
+                to="/contact" 
+                className='nav-names' 
+                activeClassName="active"
+              >
+                Contact
+              </NavLink>
+            </li>
+            <li>
+              <NavLink 
+                to="/registration" 
+                className='nav-names' 
+                activeClassName="active"
+              >
+                Registration
+              </NavLink>
+            </li>
+            <li>
+              <NavLink 
+                to="/reviews" 
+                className='nav-names' 
+                activeClassName="active"
+              >
+                Reviews
+              </NavLink>
+            </li>
+            <li>
+              <NavLink 
+                to="/inquiry" 
+                className='nav-names' 
+                activeClassName="active"
+              >
+                Inquiry
+              </NavLink>
+            </li>
           </ul>
         </nav>
         <label htmlFor="nav-toggle" className="nav-toggle-label">

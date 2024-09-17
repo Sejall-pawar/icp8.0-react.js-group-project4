8e638ed902070f0f8c93f4b99352000b682b339a
NetraWalekar <netrawalekar6@gmail.com>
2024-09-17 12:32:24 +0530

    updates

--- src/Views/Inquiry/Inquiry.js
+++ src/Views/Inquiry/Inquiry.js
@@ -3,22 +3,25 @@ import './Inquiry.css'
 import Input from "../../Components/Input/Input";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
-import Box from './box.jpg'
+import Button from './../../Components/Button/Button'
 
 function Inquiry(){
     return(
         <div>
             <Header />
             <div className="inquiry-container">
-                <div className="input-container">
-                    <div>Enter your Name:</div>
+                    <div className="box-1">
+                    <h1>Send us query</h1>
+                    <h3>Enter your Name:</h3>
                      <Input type='text' placeholder='Enter your name' />
-                     <div>Enter your Phone No:</div>
+                     <h3>Enter your Phone No:</h3>
                      <Input type='telephone' placeholder='Enter your Number'/>
-                     <div>Enter query:</div>
-                     <Input type='text' placeholder='Start typing here...'/>
-                </div>
-                <div><img src={Box} alt="img" className="box-img" /></div>
+                    </div>
+                    <div className="box-2">
+                    <h3>Enter query:</h3>
+                    <textarea type='text' placeholder='Start typing here...' className="input-box-1"/>
+                    <div className='submit'><Button type='submit' text ='Submit'/></div>
+                    </div>
             </div>
             <Footer />
         </div>

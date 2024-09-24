d1e0c511017bf09d351df81f9cc788530e174fbd
itzvishakha202 <sonawanevishakha94@gmail.com>
2024-09-24 23:00:41 +0530

    new css addded

--- src/Views/Reviews/Reviews.js
+++ src/Views/Reviews/Reviews.js
@@ -14,14 +14,14 @@ function Reviews() {
       <div className='cards-container'>
 
         <div className='card-container1'>
-      <div className='review-container'><Card img={profile} userName = 'Omkar Pawar' CustReviews = ' Such a lovely experience. The girls made the experience really enjoyable even in the current climate.Such a good experience.' Reviews='(4.5)'Card img1={rating} /></div>
-      <div className='review-container'><Card img={woman} userName = 'Netra W'  CustReviews = ' The best! Found me the perfect dress and such lovely service and attention. Not stuffy or overbearing and listen to what you want.' Reviews='(4.5)' Card img1={rating}  /></div>
-      <div className='review-container'><Card img={woman} userName = 'Tanuja S'  CustReviews = 'This app allows me to do [problem] from my smartphone where I want and when I want to.The interface is easy to navigate.' Reviews='(4.7)' Card img1={rating}  /></div>
+      <div className='review-container'><Card img={profile} userName = 'john ' CustReviews = ' Such a lovely experience. The girls made the experience really enjoyable even in the current climate.Such a good experience.' Reviews='(4.5)'Card img1={rating} /></div>
+      <div className='review-container'><Card img={woman} userName = 'sandy'  CustReviews = ' The best! Found me the perfect dress and such lovely service and attention. Not stuffy or overbearing and listen to what you want.' Reviews='(4.5)' Card img1={rating}  /></div>
+      <div className='review-container'><Card img={woman} userName = 'sammy'  CustReviews = 'This app allows me to do [problem] from my smartphone where I want and when I want to.The interface is easy to navigate.' Reviews='(4.7)' Card img1={rating}  /></div>
       </div>
       <div className='card-container2'>
-      <div className='review-container'><Card img={woman} userName = 'Varsha vanve'  CustReviews = 'Great dealer to make your first car on rent! Everything went smooth and fast,salesman did a great job informing me about the vehicle.' Reviews='(4.1)' Card img1={rating}  /></div>
-      <div className='review-container'><Card img={woman} userName = 'Sanika S'  CustReviews = ' The Sony Alpha a7 III is an outstanding camera with fantastic image quality and low-light performance.' Reviews='(4.3)' Card img1={rating}  /></div>
-      <div className='review-container'><Card img={profile} userName = 'Pratik S'  CustReviews = 'The rental fee was reasonable compared to other services. Plus, the option to extend the rental period.' Reviews='(4.1)' Card img1={rating} /></div>
+      <div className='review-container'><Card img={woman} userName = 'shikha'  CustReviews = 'Great dealer to make your first car on rent! Everything went smooth and fast,salesman did a great job informing me about the vehicle.' Reviews='(4.1)' Card img1={rating}  /></div>
+      <div className='review-container'><Card img={woman} userName = 'sobia'  CustReviews = ' The Sony Alpha a7 III is an outstanding camera with fantastic image quality and low-light performance.' Reviews='(4.3)' Card img1={rating}  /></div>
+      <div className='review-container'><Card img={profile} userName = 'robert'  CustReviews = 'The rental fee was reasonable compared to other services. Plus, the option to extend the rental period.' Reviews='(4.1)' Card img1={rating} /></div>
       </div>
       </div>
       
